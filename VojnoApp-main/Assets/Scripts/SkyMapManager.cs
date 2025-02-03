1da774352dca2e6ae72143fba58c9146f4295854
Marko Barić <markob0804@gmail.com>
2025-02-03 23:45:37 +0100

    dodan tilt

--- VojnoApp-main/Assets/Scripts/SkyMapManager.cs
+++ VojnoApp-main/Assets/Scripts/SkyMapManager.cs
@@ -19,11 +19,13 @@ public class SkyMapManager : MonoBehaviour
     public Slider offsetXSlider;
     public Slider offsetZSlider;
     public Slider rotationSlider;
+    public Slider tiltSlider;
 
 
     private float userHeightOffset = 0f;
     private Vector3 userOffset = Vector3.zero;
     private float userRotationAngle = 0f;
+    private float userTiltAngle = 0f;
     private string csvFilePath;
 
     void Start()
@@ -54,22 +56,29 @@ public class SkyMapManager : MonoBehaviour
             SaveSettings();
         });
 
+        tiltSlider.onValueChanged.AddListener((value) => { 
+            userTiltAngle = value;
+            SaveSettings();
+        });
+
         LoadSettings();
         
         heightSlider.value = userHeightOffset;
         offsetXSlider.value = userOffset.x;
         offsetZSlider.value = userOffset.z;
         rotationSlider.value = userRotationAngle;
+        tiltSlider.value = userTiltAngle;
 
         Debug.Log("Sliders initialized with values: " +
             $"Height: {heightSlider.value}, " +
             $"Offset: ({offsetXSlider.value}, {offsetZSlider.value}), " +
-            $"Rotation: {rotationSlider.value}");
+            $"Rotation: {rotationSlider.value}, " +
+            $"Tilt: {tiltSlider.value}");
     }
 
     private void SaveSettings()
     {
-        string data = $"{DateTime.Now}, {userHeightOffset}, {userOffset.x}, {userOffset.z}, {userRotationAngle}\n";
+        string data = $"{DateTime.Now}, {userHeightOffset}, {userOffset.x}, {userOffset.z}, {userRotationAngle}, {userTiltAngle}\n";
         File.AppendAllText(csvFilePath, data);
     }
 
@@ -84,8 +93,9 @@ public class SkyMapManager : MonoBehaviour
                 if (lastLine.Length == 6)
                 {
                     userHeightOffset = float.Parse(lastLine[1]);
-                    userOffset = new Vector3(float.Parse(lastLine[2]), float.Parse(lastLine[3]), float.Parse(lastLine[4]));
-                    userRotationAngle = float.Parse(lastLine[5]);
+                    userOffset = new Vector3(float.Parse(lastLine[2]), 0, float.Parse(lastLine[3]));
+                    userRotationAngle = float.Parse(lastLine[4]);
+                    userTiltAngle = float.Parse(lastLine[5]);
                 }
             }
         }
@@ -94,31 +104,10 @@ public class SkyMapManager : MonoBehaviour
     
     void Update()
     {
-        /*
-        Vector3 offset = new Vector3(
-            Mathf.Sin((_camT.eulerAngles.y + userRotationAngle) * Mathf.Deg2Rad), 
-            0, 
-            Mathf.Cos((_camT.eulerAngles.y + userRotationAngle) * Mathf.Deg2Rad)
-        ) * _radius;
-
-        Vector3 totalOffset = offset + userOffset;
-
-        Vector3 newPosition = new Vector3(
-            _playerT.position.x + totalOffset.x,
-            playerSpawnPos.y + userHeightOffset,
-            _playerT.position.z + totalOffset.z
-        );
-
-        _parentT.position = newPosition;
-        _skyMapChildT.localPosition = new Vector3(
-            skymapSpawnPos.x + (playerSpawnPos.x - _playerT.position.x),
-            skymapSpawnPos.y, // Keep original Y position
-            skymapSpawnPos.z + (playerSpawnPos.z - _playerT.position.z)
-        );
-        */
         Vector3 newPosition = playerSpawnPos + new Vector3(userOffset.x, userHeightOffset, userOffset.z);
-        Quaternion newRotation = Quaternion.Euler(0, userRotationAngle, 0);
+        Quaternion newRotation = Quaternion.Euler(userTiltAngle, userRotationAngle, 0);
         _parentT.SetPositionAndRotation(newPosition, newRotation);
+
 
         Debug.Log($"SkyMap Updated: Position {newPosition}, Rotation {newRotation.eulerAngles.y}");
     }
